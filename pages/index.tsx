0b9b0f9cd1289d945fe357ee617268e9a484bd0e
yukun5381 <yukun5381@yahoo.co.jp>
2023-02-16 00:20:22 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+          <h1>Hello World</h1>
           <Image
             className={styles.logo}
             src="/next.svg"
