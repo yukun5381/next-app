98e9001f10dfba3a70a5321e14abf000fba731ec
yukun5381 <yukun5381@yahoo.co.jp>
2023-03-14 01:06:16 +0900

    storybookが動かないエラーを解消

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,22 @@
+/* eslint-disable prettier/prettier */
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  stories: [
+  "stories": [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-postcss',
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
   ],
-  staticDirs: ['public'],
-  babel: async options => ({
-    ...options,
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-private-methods',
-      '@babel/plugin-proposal-private-property-in-object',
-    ],
-  }),
+  "staticDirs": ['public'],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
   webpackFinal: async (config) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
